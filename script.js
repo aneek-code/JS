77042304279d416dc8cf4b79b85ebcb6502bf6fc
Aneek <aneekkumardutta@gmail.com>
2026-08-17 17:01:21 +0530

    js basics

--- script.js
+++ script.js
@@ -32,22 +32,70 @@
 //     console.log(aneek[val]);
 // }
 
-let s= [1,2,3,4,5,6];
-let sum=0;
-for(let i=0; i<s.length; i++)
-{
-    sum= sum+ s[i];
-}
-console.log(sum);
+// let s= [1,2,3,4,5,6];
+// let sum=0;
+// for(let i=0; i<s.length; i++)
+// {
+//     sum= sum+ s[i];
+// }
+// console.log(sum);
 
 
-let str= prompt('enter a name');
-str= str.toLowerCase();
-let count=0;
-for(let c of str){
-    if(c=='i' || c=='a' || c=='e' || c=='u' || c=='o')
-    {
-        count++;
-    }
-}
-console.log(`the number of vowels are ${count}`);
+// let str= prompt('enter a name');
+// str= str.toLowerCase();
+// let count=0;
+// for(let c of str){
+//     if(c=='i' || c=='a' || c=='e' || c=='u' || c=='o')
+//     {
+//         count++;
+//     }
+// }
+// console.log(`the number of vowels are ${count}`);
+// console.log(window);
+let c= document.querySelector(".op");
+let f1= document.body.firstChild;
+console.dir(f1);
+console.log(c);
+
+
+let chid= document.querySelector(".op").children;
+console.log(` div child ${chid}`);
+
+let c1= document.querySelectorAll("div");
+console.log(c1);
+
+console.log('tag name: \t' + c.tagName);
+console.log('inner html: \t' +c.innerHTML);
+console.log('inner text: \t'+ c.innerText);
+
+
+let e= document.querySelector("#e1");
+
+e.innerText= e.innerText + "whataaattatat";
+
+let r= document.querySelector(".logo");
+console.log(r.getAttribute("class"));
+r.setAttribute("class", "looogo");
+console.log(r.getAttribute("class"));
+
+let head= document.querySelector("header");
+head.style.backgroundColor= "blue";
+
+head.style.fontSize= "26px";
+
+let bo= document.createElement("a");
+bo.innerText= "rat";
+bo.href="#";
+bo.style.textDecoration= "none";
+bo.style.fontSize= "32px";
+bo.style.color= "white";
+
+let an1= document.querySelector(".an1");
+an1.before(bo);
+an1.append(bo);
+
+let r1= document.querySelector(".r1");
+
+// r1.setAttribute("class", "r1v2");
+
+r1.classList.add("r1v2");
