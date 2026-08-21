979ee2d9ea1b3c9d7e843217a5952776511e9f46
Aneek <aneekkumardutta@gmail.com>
2026-08-21 15:36:20 +0530

    random things

--- jis.js
+++ jis.js
@@ -1,58 +1,79 @@
-function yo(){
-    console.log("yoooo");
+function yo() {
+  console.log("yoooo");
 }
 
 setTimeout(yo, 2000);
 
-setTimeout(()=>{
-    console.log("lag");
+setTimeout(() => {
+  console.log("lag");
 }, 3000);
 
+const data = (id, nextid) => {
+  setTimeout(() => {
+    console.log(`id is ${id}`);
+    if (nextid) {
+      nextid();
+    }
+  }, 2000);
+};
 
-
-const data= (id, nextid)=>{
-    setTimeout(()=>{
-        console.log(`id is ${id}`);
-        if(nextid){
-            nextid();
-        }
-    }, 2000);
-}
-
-data(1,()=>{
-    data(2, ()=>{
-        data(3);
-    });
+data(1, () => {
+  data(2, () => {
+    data(3);
+  });
 });
 
-let pr= new Promise((resolve, reject)=>{
+let pr = new Promise((resolve, reject) => {
+  console.log("promise");
+  resolve("success");
+});
+
+const gp = () => {
+  return new Promise((resolve, reject) => {
     console.log("promise");
     resolve("success");
-});
-
-const gp= ()=>{
-    return new Promise((resolve, reject)=>{
-        console.log('promise');
-        resolve('success');
-    });
+  });
 };
 
-let pr1= gp();
-pr1.then(()=>{
-    console.log('succesful');
+let pr1 = gp();
+pr1.then(() => {
+  console.log("succesful");
 });
 
-function hello(x){
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-        console.log(x);
-        resolve("success");
+function hello(x) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(x);
+      resolve("success");
     }, 2000);
-    });
-};
-
-async function get() {
-    await hello("modok");
-    await hello("sama");    
+  });
 }
 
+async function get() {
+  await hello("modok");
+  await hello("sama");
+}
+
+const URL = "https://time.now/developer/api/ip";
+let space = document.querySelector("#tt");
+let btn = document.querySelector("#but");
+
+async function getime() {
+  let res = await fetch(URL);
+  let data = await res.json();
+  space.innerText = data.datetime;
+}
+
+btn.addEventListener("click", getime);
+
+// let mspace = document.querySelector("#m");
+// let mbtn = document.querySelector("#meme");
+
+// const URL1 = "https://alpha-meme-maker.herokuapp.com/13";
+// async function getmeme() {
+//   let res = await fetch(URL1);
+//   let mee = await res.json();
+//   mspace.innerText = mee.data.detail;
+// }
+
+// mbtn.addEventListener("click", getmeme);
---
+++ randomscript.js
@@ -0,0 +1,77 @@
+const col_arr = [
+  "black",
+  "red",
+  "green",
+  "blue",
+  "yellow",
+  "violet",
+  "purple",
+  "orange",
+  "navy",
+  "aquamarine",
+  "white",
+];
+
+let ele1 = document.querySelector("#change-back");
+
+const change_col = () => {
+  const ran = Math.random();
+  let c1 = Math.round(ran * 10);
+  document.body.style.backgroundColor = col_arr[c1];
+};
+ele1.addEventListener("click", change_col);
+
+let dec = document.querySelector("#dec");
+let inc = document.querySelector("#inc");
+let counter = document.querySelector("#counter");
+let reset = document.querySelector("#res");
+
+let c = 0;
+dec.addEventListener("click", () => {
+  counter.innerText = --c;
+  counter_change(c);
+});
+
+inc.addEventListener("click", () => {
+  counter.innerText = ++c;
+  counter_change(c);
+});
+
+reset.addEventListener("click", () => {
+  counter.innerText = 0;
+  c = 0;
+  counter_change(c);
+});
+
+const counter_change = (c) => {
+  if (c > 0) {
+    counter.style.backgroundColor = "green";
+  } else if (c < 0) {
+    counter.style.backgroundColor = "red";
+  } else {
+    counter.style.backgroundColor = "grey";
+  }
+};
+
+let clock_dis = document.querySelector("#clock");
+setInterval(() => {
+  let clock = new Date();
+  clock_dis.innerText =
+    clock.getHours() + ":" + clock.getMinutes() + ":" + clock.getSeconds();
+}, 1000);
+
+let task = document.querySelector(".task");
+const task_code = task.innerHTML;
+let text = document.querySelector("#area");
+let done = document.querySelector("#btn");
+let add_task = document.querySelector("#add");
+
+add_task.addEventListener("click", () => {
+  let new_task = document.createElement("div");
+  new_task.innerHTML = task_code;
+  task.append(new_task);
+  let del = new_task.querySelector(".del");
+  del.addEventListener("click", () => {
+    new_task.remove();
+  });
+});
